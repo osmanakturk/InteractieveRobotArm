18b1d1c5daa6f9f515caa9fe838e44dc71d2d5bc
Osman <osmanaktrk@gmail.com>
2026-02-07 03:32:08 +0100

    The bug in main.py has been fixed.

--- jetson/main.py
+++ jetson/main.py
@@ -214,7 +214,7 @@ async def ws_status(ws: WebSocket):
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
 
 
     
